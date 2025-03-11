437e9d18485b38bd84b9fa9295ac0680ab357b27
Ghafoor <ghafoornazari@proton.me>
2025-03-11 14:13:32 +0100

    Added the main file

---
+++ transcribe.py
@@ -0,0 +1,29 @@
+import whisper
+import sys
+import os
+
+output = input("Save as? ")
+def transcribe_audio(audio_path, output_path=output, model_size="small"):
+	model = whisper.load_model(model_size)
+
+	print(f"Transcribing {audio_path}...")
+	result = model.transcribe(audio_path)
+
+
+	with open(output_path, "w", encoding="utf-8") as f:
+		f.write(result["text"])
+
+	print(f"Transcription saved to {output_path}")
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print("Usage: python transcribe.py <audio_file>")
+		sys.exit(1)
+
+	audio_file = sys.argv[1]
+
+	if not os.path.exists(audio_file):
+		print(f"Error: File '{audio_file}' not found. ")
+		sys.exit(1)
+
+	transcribe_audio(audio_file)
